df58cfcb64e76cc7531abaf5b22edc66321e082c
Kevin W Matthews <KevinWMatthews@gmail.com>
2017-04-16 16:24:44 -0700

    wip

--- test/test_client.py
+++ test/test_client.py
@@ -1,6 +1,7 @@
 import pytest
 from tftp import Client
 from mock import patch
+import socket
 
 def test_client_can_be_created():
     server_ip = '127.0.0.1'
@@ -18,3 +19,29 @@ def test_client_creates_socket(mock_socket):
     client_filename = 'tst.txt'
     Client(server_ip, client_directory, client_filename)
     assert mock_socket.called
+
+#  @patch('socket.socket.sendto')
+#  @patch('socket.socket')
+#  def test_get_sends_read_request(mock_socket, mock_method):
+#      server_ip = '127.0.0.1'
+#      client_directory = '.'
+#      client_filename = 'tst.txt'
+#      client = Client(server_ip, client_directory, client_filename)
+#      #TODO Redo with socket defined here.
+#      #  client.get()
+#      
+#      assert mock_socket.called
+#      #  assert mock_method.called
+#      mock_method.assert_called()
+
+#  def test_get_sends_read_request2():
+#      server_ip = '127.0.0.1'
+#      client_directory = '.'
+#      client_filename = 'tst.txt'
+#      client = Client(server_ip, client_directory, client_filename)
+#      with patch.object(socket.socket, 'send', return_value=None) as mm:
+#          sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#          sock.send('data')#, ('localhost, 69'))
+#      mm.assert_called()
+#      #  assert mock_socket.called
+#      #  mock_socket.sendto.assert_called()
--- tftp/client.py
+++ tftp/client.py
@@ -2,4 +2,7 @@ import socket
 
 class Client:
     def __init__(self, server_ip, client_directory, client_filename):
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def get(self):
+        pass
