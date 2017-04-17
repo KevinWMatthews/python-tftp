a55b51b3ee83188c884dd9c94eb65270b6a25c6d
Kevin W Matthews <KevinWMatthews@gmail.com>
2017-04-16 20:33:24 -0700

    Send a read request
    Octet mode only.

--- test/test_client.py
+++ test/test_client.py
@@ -1,6 +1,24 @@
 from tftp import Client
 import mock
 
+NULL_BYTE = '\x00'
+OPCODE_READ = '\x00\x01'
+
 def test_client_can_be_created():
     mock_socket = mock.Mock()
     assert Client(mock_socket)
+
+def test_send_read_request():
+    mock_socket = mock.Mock()
+
+    filename = 'filename.ext'
+
+    packet_string = OPCODE_READ
+    packet_string += filename
+    packet_string += NULL_BYTE
+    packet_string += 'octet'
+    packet_string += NULL_BYTE
+
+    client = Client(mock_socket)
+
+    assert packet_string == client.read(filename)
--- tftp/client.py
+++ tftp/client.py
@@ -1,3 +1,15 @@
+import struct
+
 class Client:
     def __init__(self, socket):
         pass
+
+    def read(self, filename):
+        format_string = '!'             # Network (big endian)
+        format_string += 'H'            # opcode - two-byte unsigned short
+        format_string += str(len(filename))
+        format_string += 's'            # filename - string
+        format_string += 'B'            # null byte - one-byte unsigned char
+        format_string += '5s'           # mode - 'octet'
+        format_string += 'B'            # null byte - one-byte unsigned char
+        return struct.pack(format_string, 1, filename, 0, 'octet', 0)
