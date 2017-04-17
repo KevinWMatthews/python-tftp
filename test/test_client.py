71e03ceab9dbede3c8d60f1a536d904b8a724bba
Kevin W Matthews <KevinWMatthews@gmail.com>
2017-04-16 17:08:28 -0700

    Basic mock

--- test/test_client.py
+++ test/test_client.py
@@ -1,5 +1,18 @@
 import pytest
-from tftp import Client
-from mock import patch
-import socket
+import mock
+from mock import MagicMock
 
+
+class ProductionClass():
+    def __init__(self):
+        pass
+
+    def method(self):
+        pass
+
+
+def test_1(mocker):
+    thing = ProductionClass()
+    thing.method = MagicMock(return_value = 66) # This also works with Mock
+    assert thing.method(42) == 66
+    thing.method.assert_called_with(42)
