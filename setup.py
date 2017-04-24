d6e1fa1f271f79bfb300cd239db25b3e897bb151
Kevin W Matthews <KevinWMatthews@gmail.com>
2017-04-23 20:27:45 -0700

    Add script to generate string of random characters, rename get to tftp_get

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
 setup(name='tftp',
-      version='0.1',
-      description='Test-drive Python TFTP library',
-      author='Kevin W Matthews',
-      author_email='KevinWMatthews@gmail.com',
-      license='MIT',
-      packages=['tftp'],
-      zip_safe=False)
+    version='0.1',
+    description='Test-drive Python TFTP library',
+    author='Kevin W Matthews',
+    author_email='KevinWMatthews@gmail.com',
+    license='MIT',
+    packages=['tftp'],
+    zip_safe=False,
+    scripts=['bin/tftp_get', 'bin/random_string'],
+)
